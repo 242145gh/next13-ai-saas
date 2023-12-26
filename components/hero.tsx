8ae87136f7e7f18645833ca167e5c06e5e3b5d38
242145gh <arrangilligan@gmail.com>
2023-12-26 09:33:10 +0100

    img

--- components/hero.tsx
+++ components/hero.tsx
@@ -12,7 +12,7 @@ export const Heros: React.FC<HeroProps> = ({ image, name,url  }) => {
 <div className="flex-shrink-0 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4">
    <a href={url}><img src={image} alt={name}  className="w-full h-full object-cover" /></a>
     <div className="text-center">   
-        <h1 className="text-violet-500 text-md sm:text-xl md:text-2xl lg:text-2xl xl:text-2xl">
+        <h1 className="text-violet-500 text-lg sm:text-xl md:text-2xl lg:text-2xl xl:text-2xl">
             {name}
         </h1>
     </div>
