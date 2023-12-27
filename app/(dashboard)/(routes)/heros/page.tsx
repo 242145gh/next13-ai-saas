00130ebbe866f7b713bab5ad2a2ceaf5c78d5e20
242145gh <arrangilligan@gmail.com>
2023-12-27 20:57:48 +0100

    messageCount

--- app/(dashboard)/(routes)/heros/page.tsx
+++ app/(dashboard)/(routes)/heros/page.tsx
@@ -21,14 +21,14 @@ const HeroPage = () => {
         iconColor="text-violet-700"
         bgColor="bg-violet-500/30" />
     </div><div className="flex flex-wrap gap-2 p-4">
-        <Heros icon={MessageCircleIcon} image="/Rocky Balboa.jpg" name="Rocky Balboa" url="/rocky" />
-        <Heros icon={MessageCircleIcon} image="/james_bond.jpg" name="James Bond (Sean Connery)" url="/bond" />
-        <Heros icon={MessageCircleIcon} image="/Sir_Winston_Churchill_-_19086236948.jpg" name="Sir Winston Churchill" url="/churchill"  />
-        <Heros icon={MessageCircleIcon} image="/arnold.jpg" name="Arnold Schwarzenegger" url="/arnold"  />
-        <Heros icon={MessageCircleIcon} image="/Xi Jinping.jpg" name="Xi Jinping" url="/jinping"  />
-        <Heros icon={MessageCircleIcon} image="/putin.jpg" name="Vladimir Putin" url="/putin"  />
-        <Heros icon={MessageCircleIcon} image="/super_woman.jpg" name="Super Woman" url="/swoman"  />
-        <Heros icon={MessageCircleIcon} image="/skeletor.jpg" name="Skeletor" url="/skeletor"  />
+        <Heros icon={MessageCircleIcon} image="/Rocky Balboa.jpg" name="Rocky Balboa" url="/rocky" MessageCount={undefined} />
+        <Heros icon={MessageCircleIcon} image="/james_bond.jpg" name="James Bond (Sean Connery)" url="/bond" MessageCount={undefined} />
+        <Heros icon={MessageCircleIcon} image="/Sir_Winston_Churchill_-_19086236948.jpg" name="Sir Winston Churchill" url="/churchill" MessageCount={undefined}  />
+        <Heros icon={MessageCircleIcon} image="/arnold.jpg" name="Arnold Schwarzenegger" url="/arnold" MessageCount={undefined}  />
+        <Heros icon={MessageCircleIcon} image="/Xi Jinping.jpg" name="Xi Jinping" url="/jinping" MessageCount={undefined}  />
+        <Heros icon={MessageCircleIcon} image="/putin.jpg" name="Vladimir Putin" url="/putin" MessageCount={undefined}  />
+        <Heros icon={MessageCircleIcon} image="/super_woman.jpg" name="Super Woman" url="/swoman" MessageCount={undefined}  />
+        <Heros icon={MessageCircleIcon} image="/skeletor.jpg" name="Skeletor" url="/skeletor" MessageCount={undefined}  />
 
       
       </div></>
