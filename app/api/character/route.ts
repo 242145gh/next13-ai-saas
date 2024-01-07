a5eb27629aec42896876f216d28f9963d10782af
242145gh <arrangilligan@gmail.com>
2024-01-07 18:52:21 +0100

    /var/task cwd

--- app/api/character/route.ts
+++ app/api/character/route.ts
@@ -30,7 +30,7 @@ async function updateCharactersInFile(newCharacter: any) {
   try {
     // Update the path to the characters.json file
     // locally this directory const filePath = './public/characters.json';
-    const filePath = '/characters.json';
+    const filePath = '/app/character/characters.json';
     // Read the current content of characters.json
     const currentCharacters = await fs.readFile(process.cwd() + filePath, 'utf-8');
     const charactersData = JSON.parse(currentCharacters);
